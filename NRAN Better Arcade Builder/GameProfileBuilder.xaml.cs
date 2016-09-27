753f36e97b8978a45a06ddbbc8a9b7e184372124
Lamphobic <kongregamer@gmail.com>
2016-09-26 23:28:58 -0700

    Lots opf Ui stuff. SOme functionality. DO NOT TEST.

--- NRAN Better Arcade Builder/GameProfileBuilder.xaml.cs
+++ NRAN Better Arcade Builder/GameProfileBuilder.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace NRAN_Better_Arcade_Builder
 {
@@ -30,5 +32,158 @@ namespace NRAN_Better_Arcade_Builder
             winOpen.Show();
             this.Close();
         }
+
+        private void GameLinkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NewRetroArcade\\Content\\Roms");
+            if (openFileDialog.ShowDialog() == true)
+            {
+                GameLinkBox.Text = System.IO.Path.GetFileName(openFileDialog.FileName);
+            }
+        }
+
+        private void EmulatorLinkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NewRetroArcade\\Content\\Emulators");
+            if (openFileDialog.ShowDialog() == true)
+            {
+                EmulatorLinkBox.Text = System.IO.Path.GetFileName(openFileDialog.FileName);
+            }
+        }
+
+        private void CartidgeArtLinkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NewRetroArcade\\Content\\Cartridges");
+            if (openFileDialog.ShowDialog() == true)
+            {
+                CartridgeArtLinkBox.Text = System.IO.Path.GetFileName(openFileDialog.FileName);
+            }
+        }
+
+        private void GameImageLinkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NewRetroArcade\\Content\\Roms");
+            if (openFileDialog.ShowDialog() == true)
+            {
+                GameImageLinkBox.Text = System.IO.Path.GetFileName(openFileDialog.FileName);
+            }
+        }
+
+        private void VertCabArtFrontLinkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NewRetroArcade\\Content\\Arcades");
+            if (openFileDialog.ShowDialog() == true)
+            {
+                VertCabArtFrontLinkBox.Text = System.IO.Path.GetFileName(openFileDialog.FileName);
+            }
+        }
+
+        private void VertCabArtSideLinkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NewRetroArcade\\Content\\Arcades");
+            if (openFileDialog.ShowDialog() == true)
+            {
+                VertCabArtSideLinkBox.Text = System.IO.Path.GetFileName(openFileDialog.FileName);
+            }
+        }
+
+        private void SpecCabArtFrontLinkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NewRetroArcade\\Content\\Arcades");
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SpecCabArtFrontLinkBox.Text = System.IO.Path.GetFileName(openFileDialog.FileName);
+            }
+        }
+
+        private void SpecCabArtSideLinkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NewRetroArcade\\Content\\Arcades");
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SpecCabArtSideLinkBox.Text = System.IO.Path.GetFileName(openFileDialog.FileName);
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e) //Load
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "profiles");
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (StreamReader readtext = new StreamReader(myStream))
+                        {
+                            // Insert code to read the stream here.
+                            GameNameBox.Text = readtext.ReadLine();
+                            GameLinkBox.Text = readtext.ReadLine();
+                            EmulatorLinkBox.Text = readtext.ReadLine();
+                            VolumeBox.Text = readtext.ReadLine();
+                            CartridgeArtLinkBox.Text = readtext.ReadLine();
+                            comboBox.SelectedItem = readtext.ReadLine();
+                            GameImageLinkBox.Text = readtext.ReadLine();
+                            VertCabArtFrontLinkBox.Text = readtext.ReadLine();
+                            VertCabArtSideLinkBox.Text = readtext.ReadLine();
+                            SpecCabArtFrontLinkBox.Text = readtext.ReadLine();
+                            SpecCabArtSideLinkBox.Text = readtext.ReadLine();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+            
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e) //Save
+        {
+            Stream myStream = null;
+            SaveFileDialog openFileDialog = new SaveFileDialog();
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "profiles");
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (StreamReader readtext = new StreamReader(myStream))
+                        {
+                            // Insert code to read the stream here.
+                            GameNameBox.Text = readtext.ReadLine();
+                            GameLinkBox.Text = readtext.ReadLine();
+                            EmulatorLinkBox.Text = readtext.ReadLine();
+                            VolumeBox.Text = readtext.ReadLine();
+                            CartridgeArtLinkBox.Text = readtext.ReadLine();
+                            comboBox.SelectedItem = readtext.ReadLine();
+                            GameImageLinkBox.Text = readtext.ReadLine();
+                            VertCabArtFrontLinkBox.Text = readtext.ReadLine();
+                            VertCabArtSideLinkBox.Text = readtext.ReadLine();
+                            SpecCabArtFrontLinkBox.Text = readtext.ReadLine();
+                            SpecCabArtSideLinkBox.Text = readtext.ReadLine();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+    }
     }
 }
