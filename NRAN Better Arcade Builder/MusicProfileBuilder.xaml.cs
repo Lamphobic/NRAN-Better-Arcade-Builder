34c370ff1af2e3c62a967fb42ebe68568e036833
Lamphobic <kongregamer@gmail.com>
2016-09-27 22:59:59 -0700

    Music Profile working.

--- NRAN Better Arcade Builder/MusicProfileBuilder.xaml.cs
+++ NRAN Better Arcade Builder/MusicProfileBuilder.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace NRAN_Better_Arcade_Builder
 {
@@ -29,6 +31,72 @@ namespace NRAN_Better_Arcade_Builder
             MainWindow winOpen = new MainWindow();
             winOpen.Show();
             this.Close();
+        }
+
+        private void GameLinkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NewRetroArcade\\Content\\Roms");
+            if (openFileDialog.ShowDialog() == true)
+            {
+                MusicLinkBox.Text = System.IO.Path.GetFileName(openFileDialog.FileName);
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "profiles");
+            openFileDialog.Filter = "Music Profiles (*.musicprofile)|*.musicprofile|Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog.DefaultExt = "gameprofile";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (StreamReader readText = new StreamReader(myStream))
+                        {
+                            // Insert code to read the stream here.
+                            MusicNameBox.Text = readText.ReadLine();
+                            MusicLinkBox.Text = readText.ReadLine();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Stream myStream = null;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "profiles");
+            saveFileDialog.Filter = "Music Profiles (*.musicprofile)|*.gameprofile";
+            saveFileDialog.DefaultExt = "musicprofile";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    if ((myStream = saveFileDialog.OpenFile()) != null)
+                    {
+                        using (StreamWriter writeText = new StreamWriter(myStream))
+                        {
+                            // Insert code to write the stream here.
+                            writeText.WriteLine(MusicNameBox.Text);
+                            writeText.WriteLine(MusicLinkBox.Text);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
         }
     }
 }
